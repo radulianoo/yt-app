0e5ae8d3e016b171d096bd7bc4eca123f31ce573
Octav Radulian <octavradulian@Octavs-MacBook-Pro.local>
2023-03-07 21:14:35 +0200

    initial setup of the project

---
+++ ytapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ytapp
+//
+//  Created by Octav Radulian on 07.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
